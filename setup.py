2c7eaaf9db904c795f5b23507320e708ea48e013
Petr Messner <petr.messner@gmail.com>
2018-12-15 13:25:19 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yalzma',
-    version='0.0.1',
+    version='0.0.2',
     description='Yet Another LZMA Python wrapper',
     classifiers=[
         'Programming Language :: Python :: 3.5',
